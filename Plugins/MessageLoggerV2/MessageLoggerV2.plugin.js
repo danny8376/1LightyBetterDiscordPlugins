9038cc9ff2d39870c33780ee973ddbb2dd506d20
DannyAAM <danny@saru.moe>
2025-02-07 23:13:36 +0800

    add heavy duty saving for MLv2

--- Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
+++ Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
@@ -352,10 +352,12 @@ module.exports = class MessageLoggerV2 {
       data = defaultConstruct();
     } else {
       data = XenoLib.loadData(this.getName() + 'Data', 'data', defaultConstruct(), true);
+      if (data && data.messageRecord === 'Split') data.messageRecord = this.loadSplitMessageRecords(this.getName() + 'DataMessageRecord');
       const isBad = map => !(map && map.messageRecord && map.editedMessageRecord && map.deletedMessageRecord && map.purgedMessageRecord && typeof map.messageRecord == 'object' && typeof map.editedMessageRecord == 'object' && typeof map.deletedMessageRecord == 'object' && typeof map.purgedMessageRecord == 'object');
       if (isBad(data)) {
         if (this.settings.autoBackup) {
           data = XenoLib.loadData(this.getName() + 'DataBackup', 'data', defaultConstruct(), true);
+          if (data && data.messageRecord === 'Split') data.messageRecord = this.loadSplitMessageRecords(this.getName() + 'DataMessageRecordBackup');
           if (isBad(data)) {
             XenoLib.Notifications.error(`[${this.getName()}] Data and backup files were corrupted. All deleted/edited/purged messages have been erased.`, { timeout: 0 });
             data = defaultConstruct();
@@ -1800,7 +1802,21 @@ module.exports = class MessageLoggerV2 {
     };
     const useEfficient = !window.ED;
     if (useEfficient) {
-      this.efficientlySaveData(
+      if (Object.keys(this.messageRecord).length > 5000) this.efficientlySaveData(
+        this.getName() + 'Data',
+        'data',
+        {
+          messageRecord: 'Split',
+          deletedMessageRecord: this.deletedMessageRecord,
+          editedMessageRecord: this.editedMessageRecord,
+          purgedMessageRecord: this.purgedMessageRecord
+        },
+        err => {
+          if (err) callback(err);
+          else this.saveSplitMessageRecords(this.getName() + 'DataMessageRecord', callback);
+        }
+      );
+      else this.efficientlySaveData(
         this.getName() + 'Data',
         'data',
         {
@@ -1827,17 +1843,420 @@ module.exports = class MessageLoggerV2 {
   efficientlySaveData(name, key, data, callback) {
     try {
       let loadedData;
-      try {
+      //try {
         /* bd gay bruh */
-        loadedData = BdApi.loadData(name, key);
-      } catch (err) { }
-      if (loadedData) for (const key in data) loadedData[key] = data[key];
-      this.nodeModules.fs.writeFile(this.__isPowerCord ? BdApi.__getPluginConfigPath(name) : this.nodeModules.path.join(this.pluginDir, `${name}.config.json`), JSON.stringify({ [key]: data }), callback);
+        //loadedData = BdApi.loadData(name, key);
+      //} catch (err) { }
+      //if (loadedData) for (const key in data) loadedData[key] = data[key];
+      //this.nodeModules.fs.writeFile(this.__isPowerCord ? BdApi.__getPluginConfigPath(name) : this.nodeModules.path.join(this.pluginDir, `${name}.config.json`), JSON.stringify({ [key]: data }), callback);
+      ((obj2stringify, asyncStringifyCallback) => {
+
+
+
+
+
+
+
+
+
+
+
+
+let counter = 0;
+let objStack = [];
+let temp = '';
+const limit = 100000;
+
+function StringifyError(m) {
+  this.name = 'Error';
+  this.message = m;
+}
+
+/**
+ * Checking for unicode and backslash characters and replaces if any.
+ * @param { string }
+ * @return { string }
+ */
+
+let normalize = (string, flagN) => {
+  let retStr = '';
+  let transform = '';
+  let uc =
+  '/[\\\'\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4' +
+  '\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g';
+  let unicode = new RegExp(uc);
+  // Taking '\\' out of the loop to avoid change in
+  // order of execution of object entries resulting
+  // in unwanted side effect
+  string = string.replace(/\\/gi, '\\\\');
+  let escape = {
+    '\b': '\\b',
+    '\t': '\\t',
+    '\n': '\\n',
+    '\f': '\\f',
+    '\r': '\\r',
+    '"': '\\"',
+  };
+  // Escape is implemented globally
+  for(var pattern in escape) {
+    var regex = new RegExp(pattern,'gi')
+    string = string.replace(regex, escape[pattern])
+  }
+  unicode.lastIndex = 0;
+  if (unicode.test(string)) {
+    // Unicode logic here
+    transform = string.replace(unicode, (a) => {
+      return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+    });
+    if (flagN === 1) {
+      transform += temp;
+      transform += transform;
+      temp = '';
+      return '"' + transform + '"';
+    } else if (flagN === 2) {
+      return '"' + transform + '"';
+    } else {
+      temp += transform;
+    }
+  } else {
+    if (flagN === 1) {
+      retStr += temp;
+      retStr += string;
+      temp = '';
+      return '"' + retStr + '"';
+    } else if (flagN === 2) {
+      return '"' + string + '"';
+    } else {
+      temp += string;
+      return;
+    }
+  }
+};
+
+/**
+ * Obtain stringified value by yielding at required intensity
+ * @param { string} field
+ * @param { primitive data type } container
+ * @param { function or array } replacer
+ * @param { number or string } space
+ * @param { number } intensity
+ * @return { function } yieldCPU
+ */
+
+function * stringifyYield(field, container, replacer, space, intensity) {
+  let itr = 0;
+  let key = '';
+  let val = '';
+  let length = 0;
+  let tempVal = '';
+  let result = '';
+  let value = container[field];
+  // Made scope local handling async issues
+  let flag1 = 0;
+  let returnStr = '';
+  let subStr = '';
+  let len = 0;
+
+  // Yield the stringification at definite intervals
+  if (++counter > 512 * intensity) {
+    counter = 0;
+    yield val;
+  }
+
+  // Call replacer if one is present (SPEC)
+  if (typeof replacer === 'function') {
+    value = replacer.call(container, field, value);
+  }
+
+  switch (typeof value) {
+    case 'string':
+      if (value.length > limit) {
+        for (let l = 0; l < value.length; l += limit) {
+          flag1 = 0;
+          yield value;
+          subStr = value.substr(l, limit);
+          len += subStr.length;
+          if (len === value.length)
+            flag1 = 1;
+          returnStr = normalize(subStr, flag1);
+        }
+      } else
+        returnStr = normalize(value, 2);
+      return returnStr;
+    case 'number':
+      return isFinite(value)
+        ? String(value)
+        : 'null';
+    case 'boolean':
+    case 'null':
+      return String(value);
+    case 'undefined':
+      return;
+    case 'object':
+      if (!value)
+        return 'null';
+
+    // Manage special cases of Arrays and Objects
+      let getResult = (decision) => {
+        if (result.length === 0)
+          if (decision)
+            return '{}';
+          else
+          return '[]';
+        else
+        if (decision)
+          if (space)
+            return '{\n' + space + result.join(',\n' + space) + '\n' + '}';
+          else
+            return '{' + result.join(',') + '}';
+        else
+          if (space)
+            return '[\n' + space + result.join(',\n' + space) + '\n' + ']';
+          else
+            return '[' + result.join(',') + ']';
+      };
+
+      result = [];
+    // If toJSON is present, invoke it (SPEC)
+      if (value && typeof value.toJSON === 'function') {
+        result.push('"' + value.toJSON(field) + '"');
+        if (result.length === 0)
+          return '{}';
+        else
+        if (space)
+          return space + result.join(',\n' + space) + '\n';
+        else
+          return result.join(',');
+      }
+    // Array case
+      if (value && value.constructor === Array) {
+        length = value.length;
+        for (itr = 0; itr < length; itr += 1) {
+          tempVal =
+          yield *stringifyYield(itr, value, replacer, space, intensity) ||
+          'null';
+          if (tempVal !== undefined)
+            result.push(tempVal);
+        }
+        return getResult(false);
+      }
+
+    // Manage replacing object scenario (SPEC)
+      if (replacer && typeof replacer === 'object') {
+        length = replacer.length;
+        for (itr = 0; itr < length; itr += 1) {
+          if (typeof replacer[itr] === 'string') {
+            key = replacer[itr];
+            val = yield *stringifyYield(key, value, replacer, space, intensity);
+            if (val !== undefined)
+              result.push(normalize(key, 2) + (space
+              ? ': '
+              : ':') + val);
+          }
+        }
+      } else {
+      // Object case
+        objStack.push(value);
+        for (key in value) {
+          if (typeof value[key] === 'object' && value[key] !== null &&
+          value[key] !== undefined) {
+            if (objStack.indexOf(value[key]) !== -1) {
+              return new StringifyError('Circular Structure Detected');
+            } else
+            objStack.push(value[key]);
+          }
+          if (Object.hasOwnProperty.call(value, key)) {
+            val = yield *stringifyYield(key, value, replacer, space, intensity);
+            if (val !== undefined)
+              result.push(normalize(key, 2) + (space
+              ? ': '
+              : ':') + val);
+          }
+          objStack = objStack.filter((v, i, a) => { return v !== value[key] });
+        }
+        objStack = objStack.filter((v, i, a) => { return v !== value });
+      }
+      return getResult(true);
+    default:
+      return new StringifyError('Unexpected Character');
+  }
+}
+
+/**
+ * Calling appropriate functions each time.
+ * @param { primitive data types } value
+ * @param { function or array } replacer
+ * @param { number or string } space
+ * @param { number } intensity
+ * @param { function } callback
+ * @return { function } yieldCPU
+ */
+
+let stringifyWrapper = (value, replacer, space, intensity, callback) => {
+  let indent = '';
+  if (typeof space === 'number') {
+    indent = ' '.repeat(space);
+  } else if (typeof space === 'string') {
+    indent = space;
+  }
+
+  let yielding;
+
+  // To hold 'stringifyYield' genarator function
+  function * yieldBridge() {
+    yielding = yield *stringifyYield('', {'': value}, replacer, indent, 1);
+  }
+
+  let rs = yieldBridge();
+  let g = rs.next();
+
+  let yieldCPU = () => {
+    setImmediate(() => {
+      g = rs.next();
+      if (g && g.done === true) {
+        // Reinitializing the values at the end of API call
+        counter = 0;
+        temp = ''
+        objStack = [];
+        if (typeof yielding === 'object')
+          return callback(yielding, null);
+        else
+          return callback(null, yielding);
+      }
+      yieldCPU();
+    });
+  };
+  return yieldCPU();
+};
+
+  function stringifyAsync(data, replacer, space, intensity, callback) {
+    const argv = arguments;
+    if (typeof argv[argv.length - 1] === 'function') {
+      callback = argv[argv.length - 1];
+      replacer = null;
+      intensity = 1;
+    } else
+      throw new TypeError('Callback is not a function');
+    if (argv.length > 2) {
+      let i = 1;
+      if (typeof argv[i] === 'function' || typeof argv[i] === 'object')
+        replacer = argv[i++];
+      if ((typeof argv[i] === 'number' || typeof argv[i] === 'string') &&
+           typeof argv[i++] === 'number')
+        space = validateSpace(argv[i++]);
+      if (typeof argv[i] === 'number')
+        intensity = validateIntensity(argv[i]);
+    }
+    return stringifyWrapper(data, replacer, space, intensity, callback);
+  }
+
+  stringifyAsync(obj2stringify, asyncStringifyCallback);
+
+
+
+
+
+
+
+
+
+
+
+
+      })({ [key]: data }, async (err, json) => {
+          if (err) ZeresPluginLibrary.Logger.err(this.getName(), 'There has been an error stringifying data for saving to the data file', err);
+          else {
+      async function asyncWrite(stream, data) {
+        const chunkSize = 1024*1024; // 1MiB chunk
+        for (let i=0; i < data.length; i += chunkSize) await new Promise(resolve => {
+          setTimeout(() => {
+            stream.write(data.substr(i, chunkSize));
+            resolve();
+          }, 0);
+        });
+      }
+      try {
+        let stream = this.nodeModules.fs.createWriteStream(this.__isPowerCord ? BdApi.__getPluginConfigPath(name) : this.nodeModules.path.join(this.pluginDir, `${name}.config.json`));
+        await asyncWrite(stream, json);
+        stream.end();
+        callback();
+      } catch (e) {
+        callback(e);
+      }
+          }
+      });
     } catch (e) {
       XenoLib.Notifications.error('There has been an error saving the data file');
       ZeresPluginLibrary.Logger.stacktrace(this.getName(), 'There has been an error saving the data file', e);
     }
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  loadSplitMessageRecords(name) {
+    // this.__isPowerCord not init yet
+    let __isPowerCord = !!window.powercord && typeof BdApi.__getPluginConfigPath === 'function' || typeof global.isTab !== 'undefined';
+    if (__isPowerCord) return XenoLib.loadData(`${name}.NoSplit`, 'split', {}, true);
+    else {
+      const messageRecord = {};
+      for (const filename of this.nodeModules.fs.readdirSync(this.nodeModules.path.join(this.pluginDir, name))) {
+        if (!filename.endsWith(".config.json")) continue;
+        console.log(filename);
+        const split = JSON.parse(this.nodeModules.fs.readFileSync(this.nodeModules.path.join(this.pluginDir, name, filename))).split;
+        for (const messageId in split) messageRecord[messageId] = split[messageId];
+      }
+      return messageRecord;
+    }
+  }
+  // this can cause desynced messagerecords, but i believe it's handled... hope so
+  async saveSplitMessageRecords(name, callback) {
+    if (this.__isPowerCord) this.efficientlySaveData(`${name}.NoSplit`, 'split', split, callback);
+    else try {
+      const path = this.nodeModules.path.join(this.pluginDir, name);
+      if (this.nodeModules.fs.existsSync(path)) this.nodeModules.fs.rmdirSync(path, { recursive: true });
+      this.nodeModules.fs.mkdirSync(path);
+      let count = 0;
+      let split = {};
+      for (const messageId in this.messageRecord) {
+        count++;
+        split[messageId] = this.messageRecord[messageId];
+        const splitSize = 1000; // 10000;
+        if (count % splitSize == 0) {
+          await new Promise((resolve, reject) => {
+            setTimeout(() => {
+              //this.efficientlySaveData(this.nodeModules.path.join(name, `Split.${Math.floor(count / splitSize)}`), 'split', split, err => {
+              //  if (err) reject(err);
+              //  else resolve();
+              //});
+              try {
+                this.nodeModules.fs.writeFileSync(this.nodeModules.path.join(this.pluginDir, name, `Split.${Math.floor(count / splitSize)}.config.json`), JSON.stringify({ split }));
+                resolve();
+              } catch (e) {
+                reject(e);
+              }
+            }, 0);
+          });
+          split = {};
+        }
+      }
+      callback();
+    } catch (e) {
+      callback(e);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
   saveBackup() {
     const callback = err => {
       if (err) {
@@ -1846,11 +2265,25 @@ module.exports = class MessageLoggerV2 {
       }
       this.saveBackupTimeout = 0;
       this.autoBackupSaveInterupts = 0;
-      if (!XenoLib.loadData(this.getName() + 'DataBackup', 'data').messageRecord) this.saveBackupTimeout = setTimeout(() => this.saveBackup, 300); // don't be taxing
+      //if (!XenoLib.loadData(this.getName() + 'DataBackup', 'data').messageRecord) this.saveBackupTimeout = setTimeout(() => this.saveBackup, 300); // don't be taxing
     };
     const useEfficient = !window.ED;
     if (useEfficient) {
-      this.efficientlySaveData(
+      if (Object.keys(this.messageRecord).length > 5000) this.efficientlySaveData(
+        this.getName() + 'DataBackup',
+        'data',
+        {
+          messageRecord: 'Split',
+          deletedMessageRecord: this.deletedMessageRecord,
+          editedMessageRecord: this.editedMessageRecord,
+          purgedMessageRecord: this.purgedMessageRecord
+        },
+        err => {
+          if (err) callback(err);
+          else this.saveSplitMessageRecords(this.getName() + 'DataMessageRecordBackup', callback);
+        }
+      );
+      else this.efficientlySaveData(
         this.getName() + 'DataBackup',
         'data',
         {
